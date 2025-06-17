55f888995770947e45e4e1309a5b922641f91963
Rudzii16 <rudziramdani5@gmail.com>
2025-06-17 23:02:02 +0700

    menambahkan search form pencarian dan tampilan kode di bawah gambar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 // Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 const hamburger = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 hamburger.addEventListener('click', function () {
   navbarNav.classList.toggle('active');
 });
 
-// Klik di luar nav untuk nutup menu (opsional tapi keren)
+// Toggle Class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar nav & search form untuk nutup semua
 document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
   }
 });
