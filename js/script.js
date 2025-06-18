e7e599ce7bb1d31a58cab0bdfdcde4b464a2eb64
Rudzii16 <rudziramdani5@gmail.com>
2025-06-18 14:24:43 +0700

    menambahkan icon keranjang belanja/shopping-cart saat di klik

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const navbarNav = document.querySelector('.navbar-nav');
 const hamburger = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 hamburger.addEventListener('click', function () {
   navbarNav.classList.toggle('active');
@@ -11,18 +12,29 @@ hamburger.addEventListener('click', function () {
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
 
-document.querySelector('#search-button').onclick = (e) => {
+sb.onclick = function (e) {
   searchForm.classList.toggle('active');
   searchBox.focus();
   e.preventDefault();
 };
 
-// Klik di luar nav & search form untuk nutup semua
+// Toggle Class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+sc.onclick = function (e) {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+// Klik di luar nav, search, dan cart untuk menutup semua
 document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
   }
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove('active');
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
   }
 });
