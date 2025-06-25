35df4446756899c9308dd03b8f6e2d44ffe57bfb
Rudzii16 <rudziramdani5@gmail.com>
2025-06-25 16:08:32 +0700

    menambahkan format angka ke rupiah bagian shopping-cart

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 document.addEventListener('alpine:init', () => {
-Alpine.data('products', () => ({
-items : [
-    { id: 1, name: ' Kenangan Milk Tea', img: 'kenangan milk tea .jpeg', price: 30000 },
-    { id: 1, name: ' Tiramisu Latte', img: 'tiramisu-latee.jpg', price: 35000 },
-    { id: 1, name: ' Vanilla Latte', img: 'vanilla-late', price: 45000 },
-],
-}));
+  Alpine.data('products', () => ({
+    items: [
+      { id: 1, name: 'Kenangan Milk Tea', img: 'kenangan-milk-tea.jpeg', price: 30000, stars: 4 },
+      { id: 2, name: 'Tiramisu Latte', img: 'tiramisu-latee.jpg', price: 35000, stars: 5 },
+      { id: 3, name: 'Vanilla Latte', img: 'vanilla-late.jpeg', price: 45000, stars: 4 },
+    ],
+  }));
+
+  Alpine.store('cart', {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      console.log(newItem);
+    },
+  });
 });
 
+
 // Konversi ke Rupiah
-const rupiah = (number) => {
-    return new Intl.NumberFormat('id-ID', {
-        style: 'currency',
-        currency: 'IDR',
-        minimumIntegerDigits: 0,
-    }).format(number);
-};
+function rupiah(number) {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR'
+  }).format(number);
+}
+
+document.addEventListener('alpine:initialized', () => {
+  Alpine.effect(() => {
+    feather.replace();
+  });
+});
